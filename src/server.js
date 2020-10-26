2e016c0ebb551b1e78a385839d94df7270e377c3
wanghong <34284143@qq.com>
2020-10-26 17:49:13 +0800

    添加koa中间件

--- src/server.js
+++ src/server.js
@@ -1,8 +1,16 @@
-const koa =  require('koa')
+const koa = require("koa");
 const app = new koa();
+const Router = require("@koa/router");
 
-app.use(async ctx=>{
-  ctx.body = 'Hello World'
-})
-
-app.listen(3000,()=>{console.log('端口3000')})
+const router = new Router();
+// app.use(async ctx=>{
+//   ctx.body = 'Hello World'
+// })
+router.get("/test", async (ctx, next) => {
+  // ctx.router available
+   ctx.body = "Hello koa";
+});
+app.use(router.routes()).use(router.allowedMethods());
+app.listen(3000, () => {
+  console.log("端口3000");
+});
