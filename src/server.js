b3e7cbf12bf9647605734586a5597206391273fe
wanghong <34284143@qq.com>
2020-10-26 17:56:41 +0800

    使用rest client 请求接口"

--- src/server.js
+++ src/server.js
@@ -10,6 +10,18 @@ router.get("/test", async (ctx, next) => {
   // ctx.router available
    ctx.body = "Hello koa";
 });
+
+router.post("/post", async (ctx, next) => {
+  // ctx.router available
+   ctx.body = "post add";
+});
+
+router.get("/post", async (ctx, next) => {
+  // ctx.router available
+   ctx.body = "post";
+});
+
+
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(3000, () => {
   console.log("端口3000");
